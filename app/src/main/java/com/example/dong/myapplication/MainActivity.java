e1ffc77196a957692496ffdb925d60606eb6be35
Villiage <changyadongholo@163.com>
2016-02-23 14:14:34 +0800

    ScrollView

--- app/src/main/java/com/example/dong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dong/myapplication/MainActivity.java
@@ -1,31 +1,56 @@
 package com.example.dong.myapplication;
 
+import android.annotation.TargetApi;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(getApplicationContext(), "Toast", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+
+        });
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent();
+                it.setClass(MainActivity.this, ScrollingActivity.class);
+                startActivity(it, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+//             startActivity(it);
             }
         });
+
     }
 
     @Override
